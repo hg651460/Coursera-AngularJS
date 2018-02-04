ddb87e8413cacb641144f375a993b10e8390a9d3
hg651460 <harshaghaturle@gmail.com>
2018-02-04 00:33:55 -0500

    assignment1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,8 @@ angular.module('Assignment1', [])
 LunchCheckController.inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.menu = "";
+  $scope.message = "";
+  $scope.txtmessage = "";
 
   $scope.numericLength = function () {
     var totalMenuValue = $scope.menu.split(",").length;
@@ -17,10 +19,16 @@ function LunchCheckController($scope) {
     var totalStringValue = string;
 
     if ($scope.menu.length < 1) {
+        $scope.message = "error";
+        $scope.txtmessage = "txterror";
       return "Please enter data first!";
     } else if (totalStringValue < 4) {
+        $scope.message = "enjoy";
+        $scope.txtmessage = "txtenjoy";
       return "Enjoy!";
     } else {
+        $scope.message = "enjoy";
+        $scope.txtmessage = "txtenjoy";
       return "Too much!";
     }
 
